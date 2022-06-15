641ae9d5cde0f8d08be24728359d75c5263f4ee1
ricardocamargo07 <53793788+ricardocamargo07@users.noreply.github.com>
2022-06-15 16:01:37 -0300

    ajustments

--- Pisicologa Cris Camargo/script.js
+++ Pisicologa Cris Camargo/script.js
@@ -1,6 +1,38 @@
-const toggleButton = document.getElementsByClassName('toggle-button')[0]
-const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+const toggleButton = document.getElementsByClassName("toggle-button")[0];
+const navbarLinks = document.getElementsByClassName("navbar-links")[0];
 
-toggleButton.addEventListener('click', () => {
-  navbarLinks.classList.toggle('active')
-})
+toggleButton.addEventListener("click", () => {
+  navbarLinks.classList.toggle("active");
+});
+
+// window.addEventListener("scroll", (event) => {
+//   var liveWidth = window.pageXOffset;
+//   var liveHeight = window.pageYOffset;
+
+//     if(liveWidth< 930 && liveHeight > 960){
+//       toggleButton.style.display = "none";
+//     } else if (liveWidth > 930){
+//       toggleButton.style.display = "none";
+//     }
+// });
+
+
+//Get the button:
+mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
